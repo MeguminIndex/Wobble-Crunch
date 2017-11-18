1d258f9115a4c6875786e1d89b8d49f1d8947f0a
HikariKuraku <oliverbeck_3@hotmail.com>
2017-11-18 20:10:08 +0000

    Added GameController

---
+++ Wobble & Crunch/Assets/Scripts/GameController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	WobbleStatScript wobbleStats;
+	
+	[SerializeField] private string[] events;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		wobbleStats = GetComponent<WobbleStatScript>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	// Methods an event may call, the index is the position in the Events array.
+	void EventListing(int index)
+	{
+		if (index == 0)
+		{
+			
+		}
+		else if (index == 1)
+		{
+			
+		}
+		else if (index == 2)
+		{
+			
+		}
+		else if (index == 3)
+		{
+			
+		}
+		else if (index == 4)
+		{
+			
+		}
+		else if (index == 5)
+		{
+			
+		}
+		
+		
+	}
+	
+	void CallEvent(string name)
+	{
+		for (int i = 0; i < events.Length; i++)
+		{
+			if (events[i] == name)
+			{
+				EventListing(i);
+			}
+		}
+	}
+}
