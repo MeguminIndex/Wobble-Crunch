63ce91bb4eb86c04311752cd9c3be8ccc6093276
Dave <dave@gmail.com>
2017-11-18 14:56:26 +0000

    Puzzles Currently

---
+++ Wobble & Crunch/Assets/McClay - Puzzles/Scripts/Placeholder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placeholder : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetKey("k"))
+            GameControls.Respawn(gameObject);
+    }
+
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Deathplane"))
+        {
+            Debug.Log("Ded");
+            GameControls.Respawn(gameObject);
+        }
+    }
+
+}
