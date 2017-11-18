9314fd24e93814ddc40faadb8bac89e714dd9882
MeguminIndex <catalyst.pw@googlemail.com>
2017-11-18 17:07:17 +0000

    Movement, Crushing
    Movement variables created for vertical and jump as well as stoping endless jumping when not on floor

--- Wobble & Crunch/Assets/Scripts/Crunch/CrushingScript.cs
+++ Wobble & Crunch/Assets/Scripts/Crunch/CrushingScript.cs
@@ -8,6 +8,8 @@ public class CrushingScript : MonoBehaviour {
     bool crushing;
     Rigidbody rb;
 
+    [SerializeField]
+    private float downForce;
 
 
 	// Use this for initialization
@@ -18,20 +20,15 @@ public class CrushingScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if(Input.GetKey(KeyCode.C))
+        if(Input.GetKeyDown(KeyCode.C))
         {
             Debug.Log("Crushiin....");
-
+            rb.AddForce(new Vector3(0, -downForce,0),ForceMode.Impulse);
             crushing = true;
-            rb.mass = 100; 
+           // rb.mass = 1; 
 
         }
-        else
-        {
-           
-            rb.mass = 1;
-            crushing = false;
-        }
+        
 		
 	}
 
@@ -47,11 +44,26 @@ public class CrushingScript : MonoBehaviour {
                 if (crushS != null)
                 {
                     crushS.Crush();
+
+
                 }
 
             }
+
+            rb.mass = 1;
+            crushing = false;
         }
 
+        
+    }
 
+
+    void OnCollisionStay(Collision collision)
+    {
+        if(crushing)
+        {
+           // rb.mass = 1;
+            crushing = false;
+        }
     }
 }
