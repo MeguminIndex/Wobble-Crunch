f71210119aef439c6df2328b7d8519825b3821c4
MeguminIndex <catalyst.pw@googlemail.com>
2017-11-19 06:51:03 +0000

    Water scroll, Moving platform texture

---
+++ Wobble & Crunch 2.0/Assets/Scripts/RotateAroundTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundTarget : MonoBehaviour {
+
+    public Transform target;
+    public float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!target)
+            return;
+
+
+        transform.LookAt(target);
+        transform.Translate((Vector3.right * rotateSpeed) * Time.deltaTime);
+
+    }
+}
---
+++ Wobble & Crunch 2.0/Assets/Scripts/offSetScroller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class offSetScroller : MonoBehaviour {
+
+    public float scrollSpeed;
+    private Vector2 savedOffset;
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        savedOffset = rend.sharedMaterial.GetTextureOffset("_MainTex");
+    }
+
+    void Update()
+    {
+        float y = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        Vector2 offset = new Vector2(savedOffset.x, y);
+        rend.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+
+    void OnDisable()
+    {
+        rend.sharedMaterial.SetTextureOffset("_MainTex", savedOffset);
+    }
+
+}
