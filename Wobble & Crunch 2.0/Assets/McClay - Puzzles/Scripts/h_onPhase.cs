ee7ba9f4a7cfac13e53345e2e35f5400e80a234e
McClay <dave@gmail.com>
2017-11-19 08:25:13 +0000

    CATAPULT

---
+++ Wobble & Crunch 2.0/Assets/McClay - Puzzles/Scripts/h_onPhase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class h_onPhase : MonoBehaviour {
+
+
+    public bool h_on = false;
+    Collider sphere;
+
+    // Use this for initialization
+    void Start () {
+        sphere = gameObject.GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (h_on)
+            sphere.enabled = false;
+
+        else
+            sphere.enabled = true;
+	}
+}
