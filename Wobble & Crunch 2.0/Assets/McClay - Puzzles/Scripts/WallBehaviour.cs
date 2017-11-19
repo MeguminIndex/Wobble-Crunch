f70719a50486213a210201f3d52c4a3d51dd0621
McClay <dave@gmail.com>
2017-11-19 07:38:15 +0000

    PAIN IN THE LAVA

--- Wobble & Crunch 2.0/Assets/McClay - Puzzles/Scripts/WallBehaviour.cs
+++ Wobble & Crunch 2.0/Assets/McClay - Puzzles/Scripts/WallBehaviour.cs
@@ -5,49 +5,79 @@ using UnityEngine;
 
 public class WallBehaviour : MonoBehaviour {
 
-    public float pos;
+    public bool invert;
+    int inverter = 1;
+    float speed = .3f;
     public int height = 10;
     Quaternion set;
     Vector3 posit;
     Rigidbody rb;
     public bool v_on = false;
     public bool h_on = false;
+    public bool z_on = false;
+    public float stop;
+    public float distance;
+    Vector3 location;
 
 
-	// Use this for initialization
-	void Start () {
-        pos = transform.position.y;
+    // Use this for initialization
+    void Start () {
+        if (invert)
+        {
+            speed = speed * -1;
+            inverter = -1;
+        }
+        location = transform.position;
 
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (pos < 0 - height - 1)
+        if (transform.position.y < (0 - height - 1)*inverter)
             v_on = false;
 
         if (v_on)
+        {
             Vertical();
+        }
 
-        if (pos < 0 - height - 1)
+        if (transform.position.x < (0 - height - 1) * inverter)
             h_on = false;
 
         if (h_on)
+        {
             Horizontal();
-        
-	}
+        }
+
+        if (stop > distance)
+            z_on = false;
+
+        if (z_on)
+        {
+            ZHorizontal();
+        }
+
+
+    }
+
+
+    public void ZHorizontal()
+    {
+        stop += speed;
+        posit = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed);
+        this.transform.SetPositionAndRotation(posit, set);
+    }
 
     public void Horizontal()
     {
-        pos -= .3f;
-        posit = new Vector3(pos, transform.position.y, transform.position.z);
+        posit = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
         this.transform.SetPositionAndRotation(posit, set);
     }
 
     public void Vertical()
     {
-        pos -= .3f;
-        posit = new Vector3(transform.position.x, pos, transform.position.z);
+        posit = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
         this.transform.SetPositionAndRotation(posit, set);
     }
 }
